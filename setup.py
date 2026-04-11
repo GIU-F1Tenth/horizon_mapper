00549bd2176a2f5663fa29716872f4b4cb539887
mohammed-azab <mo7ammed3zab@outlook.com>
2026-04-11 14:42:08 +0200

    Refactor data_files structure in setup.py for clarity and maintainability

--- setup.py
+++ setup.py
@@ -1,27 +1,20 @@
 from setuptools import find_packages, setup
-import os
 from glob import glob
 
 package_name = 'horizon_mapper'
+
+data_files = [
+    ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+    ('share/' + package_name, ['package.xml']),
+    ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+    ('share/' + package_name + '/config', glob('config/*.yaml')),
+]
 
 setup(
     name=package_name,
     version='1.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        # Include data files
-        ('share/' + package_name + '/data', [
-            'horizon_mapper/optimal_trajectory.csv',
-            'horizon_mapper/ref_trajectory.csv'
-        ]),
-        # Include launch files
-        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
-        # Include config files if any
-        ('share/' + package_name + '/config', glob('config/*.yaml')),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='mohammedazab',
